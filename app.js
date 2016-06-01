a61822b527cb3cf1ed6de6c05f1fe82f7e45c83b
TAK <infernal-slam@hotmail.com>
2016-06-01 22:33:32 +0700

    test

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
---
+++ public/todo.js
@@ -0,0 +1,8 @@
+/*global angular*/
+angular.module('todoApp', ['angularVideoBg'])
+  .controller('TodoListController', function ($scope) {
+    $scope.name = 'angularVideoBg'
+    $scope.video = {
+      id:'Fgbba5w9KVY'
+    }
+  })
